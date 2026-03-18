95969e6c5ef237fb8fa057321bda04772c461685
Nikhil Sangamkar <kenechichii@gmail.com>
2026-03-18 10:25:35 -0400

    Ediited MT6701 Driver

--- Software/Core/Inc/MT6701.h
+++ Software/Core/Inc/MT6701.h
@@ -4,30 +4,34 @@
 #include "main.h"
 #include "stm32f3xx_hal_i2c.h"
 
-
 /*
     MT6701 Register Map
-    - 0x03 (Angle Register [13:6]) Reads the highest 8 bits of the 14 bit angle measurment.
-    - 0x04 (Angle Register [5:0]) Reads the lower 6 bits of the 14 bit angle measurment.
+    - 0x03 (Angle Register [13:6]) Reads the highest 8 bits of the 14 bit angle
+   measurment.
+    - 0x04 (Angle Register [5:0]) Reads the lower 6 bits of the 14 bit angle
+   measurment.
 */
-#define MT6816_REG_ANGLE_H      0x03
-#define MT6816_REG_ANGLE_L      0x04
+#define REG_ANGLE_H 0x03
+#define REG_ANGLE_L 0x04
 
 /* I2C Address */
 #define MT6701_I2C_ADRESS 0x06
 
 /* 14-bit resolution */
-#define MT6816_RESOLUTION       16384.0f
-#define MT6816_DEGREES          360.0f
+#define MT6701_RESOLUTION 16384.0f
+#define MT6701_DEGREES 360.0f
 
 typedef struct {
-    I2C_HandleTypeDef * I2Chandle;
-    uint16_t           raw_position;
-    float              position;
-    HAL_StatusTypeDef  hal;
+  I2C_HandleTypeDef *I2Chandle;
+  uint16_t raw_position;
+  uint16_t raw_rpm;
+  float position;
+  float rpm;
+  HAL_StatusTypeDef hal;
 } MT6701;
 
-HAL_StatusTypeDef MT6701_Initialize(MT6701 *encoder, I2C_HandleTypeDef *I2Chandle,
+HAL_StatusTypeDef MT6701_Initialize(MT6701 *encoder,
+                                    I2C_HandleTypeDef *I2Chandle,
                                     GPIO_TypeDef *interrupt);
 HAL_StatusTypeDef MT6701_Read(MT6701 *encoder);
 
--- Software/Core/Src/MT6701.c
+++ Software/Core/Src/MT6701.c
@@ -3,6 +3,18 @@
 HAL_StatusTypeDef MT6701_Initialize(MT6701 * encoder, I2C_HandleTypeDef *I2Chandle)
 {
     encoder->I2Chandle = I2Chandle;
-    
+    encoder->raw_position = 0;
+    encoder->position = 0;
+    encoder->raw_rpm = 0;
+    encoder->rpm = 0;
     return encoder->hal;
+}
+
+HAL_StatusTypeDef MT6701_Read(MT6701 *encoder)
+{
+    uint8_t rxBuf[1];
+    HAL_I2C_Mem_Read(encoder->I2Chandle, MT6701_I2C_ADRESS, REG_ANGLE_H, I2C_MEMADD_SIZE_8BIT, rxBuf, 1, 100);
+    encoder->raw_position = rxBuf[0] << 8;
+    HAL_I2C_Mem_Read(encoder->I2Chandle, MT6701_I2C_ADRESS, REG_ANGLE_L, I2C_MEMADD_SIZE_8BIT, rxBuf, 1, 100);
+    encoder->raw_position += rxBuf[0];
 }
